b9c53dc994af7c370ed8732e8daae23525ad07ce
Pragya Mishra <mishrapragya9162@gmail.com>
2026-04-23 14:36:31 +0530

    model:added library.js

--- models/Library.js
+++ models/Library.js
@@ -1,1 +1,17 @@
 const mongoose=require("mongoose");
+const librarySchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,      
+    },
+    location:{
+        type:String,    
+
+    },
+    established:{
+        type:Number,
+        min:1000
+    }
+},{
+    timestamps:true         
+})
